5473949ef70fd2f6ab310cd13b80b6f04d73e612
Gregory Roman <greg@gregorys-mbp.lan>
2019-10-19 20:24:33 -0400

    Use env file.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 /* https://github.com/klarstil/lifx-http-api */
 const lifx = require('lifx-http-api');
 const client = new lifx({
-  bearerToken: 'c94c176a3c6a7cb4b8f681abb509826ee4913611195e805cb5113f4341699ad0',
+  bearerToken: process.env.BEARER_TOKEN,
 });
 
 
@@ -10,9 +12,10 @@ client
   .then((lights) => {
     console.log(lights[0]);
 
-    client.setState(`id:${lights[0].id}`, {
-      color: 'blue saturation:0.5',
-      brightness: .75,
+    // client.setState(`id:${lights[0].id}`, {
+    client.setState('all', {
+      color: 'kelvin:2700',
+      brightness: .5,
     })
       .then((message) => {
         console.log(message)
