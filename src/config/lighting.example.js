e9aa8f0e7d74adb64c853aa391408a15f364f33a
Gregory Roman <gregorymichaelroman@gmail.com>
2021-01-01 20:13:38 -0500

    Remove lighting config.

---
+++ src/config/lighting.example.js
@@ -0,0 +1,26 @@
+export default {
+  all: {
+    title: 'All Lights',
+    selector: 'all',
+  },
+  color: {
+    title: 'Color Bulb',
+    selector: 'id:123',
+  },  
+  bedroom: {
+    title: 'Bedroom',
+    lights: {
+      1: 'id:123',
+      2: 'id:123',
+    }     
+  },    
+  office: {
+    title: 'Office',
+    lights: {
+      1: 'id:123',
+      2: 'id:123',
+      3: 'id:123',
+      4: 'id:123',
+    }
+  },
+};
